411cf61398e5c32bea4d487b34aaddd3b9688b66
Karlah333 <kong.karlah@gmail.com>
2020-07-21 14:34:19 +0800

    [chore][Karlah] create new file

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -6,6 +6,7 @@ import Login from "./components/login";
 import Latest from "./components/latest";
 import Browse from "./components/browse";
 import Contact from "./components/contact";
+import ViewProfile from "./components/viewprofile";
 import { Switch, Route } from "react-router-dom"; //to be able to switch to different pages
 
 function App() {
---
+++ FrontEnd/src/components/viewprofile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function viewprofile() {
+  return (
+    <>
+      <div>
+        <h1>This is the profile page</h1>
+      </div>
+    </>
+  );
+}
+export default viewprofile;
